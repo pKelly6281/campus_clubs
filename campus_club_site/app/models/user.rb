dea85d3e471e23374ffd78e3ad76a7c0ef08f9f8
NumberJuan617 <juanluisandujar10@gmail.com>
2015-03-03 10:39:11 -0500

    adding usermodel validation

--- campus_club_site/app/models/user.rb
+++ campus_club_site/app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ActiveRecord::Base
+  	before_save { self.email = email.downcase }#runs before we execute code before
+
+	#first name and last name validation 
 	validates :firstname, presence: true, length: { maximum: 30}
 	validates :lastname, presence: true, length: { maximum: 30}
 
-	#email validation 
-	validates :email, presence:true, length: { maximum: 150}
-	#VALID_EMAIL_REGEX = /\a[\W+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+
+	#email validation 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 150 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+
+    
+    has_secure_password
+	validates :password, length: { minimum: 6 }
+
+	#has secure password links us to the password object on this line
+	#creating user password confirmation. Most of our code for making secure passwords comes from the rails method below
 end
--- campus_club_site/test/models/user_test.rb
+++ campus_club_site/test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+   def setup
+    @user = User.new(firstname: "Example User",lastname:"andujar", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+    test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
