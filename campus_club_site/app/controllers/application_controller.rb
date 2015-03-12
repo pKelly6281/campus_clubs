44a344f6223a1e9e95a18367e75919b8c7db6f95
Patrick Kelly <pk6281@mcla.edu>
2015-03-12 10:50:08 -0400

    Started work on adding clubs

--- campus_club_site/app/controllers/application_controller.rb
+++ campus_club_site/app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include  SessionsHelper
+  def render_404
+  	raise ActionController::RoutingError.new('Not Found')
+  end 
+  def notLoggedIn
+  	redirect_to login_url
+  end 
 end
--- campus_club_site/app/controllers/club_controller.rb
+++ campus_club_site/app/controllers/club_controller.rb
@@ -6,5 +6,17 @@ class ClubController < ApplicationController
   end
 
   def add
+  	if logged_in?
+      @club = Club.new
+  	else
+  		notLoggedIn
+  	end
   end
+  def create
+    @user = Club.new(club_params)
+  end 
+  private
+  def club_params
+      params.require(:club).permit(:name,:desc, :meetDate, :meetTime, :pres, :vicePres, )
+    end
 end
