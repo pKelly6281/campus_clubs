6211fc53878bad00bf545f85f5c7280890091d02
NumberJuan617 <juanluisandujar10@gmail.com>
2015-04-12 21:33:50 -0400

    Further changes added eboard club model

--- campus_club_site/app/controllers/eboards_controller.rb
+++ campus_club_site/app/controllers/eboards_controller.rb
@@ -1,6 +1,6 @@
 class EboardsController < ApplicationController
   def show
-    #shows the clubs individual page
+    #shows the eboard individual page
     @eboard = Eboard.find(params[:id])
     @user = current_user
   end
@@ -18,18 +18,27 @@ class EboardsController < ApplicationController
     if @eboard.save
       render 'show'
     else
-      #render 'new'#show error validation 
+      render 'new'#show error validation 
     end   
   end
 
  
   def edit
+    @eboard = Eboard.find(params[:id])
   end
 
   def update
+    @eboard = Eboard.find(params[:id])
+    if @eboard.update_attributes(eboard_params)
+      flash[:success] = "Executive Board has been updated. Notifications have been sent to the E-board members"
+      redirect_to @eboard
+    else
+      render 'edit'
+    end
   end 
 
   private
+  
   def eboard_params
       params.require(:eboard).permit(:president,:vice_president, :secretary, :treasurer)
   end
--- campus_club_site/app/models/eboard.rb
+++ campus_club_site/app/models/eboard.rb
@@ -1,2 +1,15 @@
 class Eboard < ActiveRecord::Base
+	#before saving conditions?
+	#sbefore_save {self.name = name.downcase}
+	#validate eboard variables 
+	validates :president, presence: true, length: { maximum: 50}
+	validates :vice_president, presence: true, length: { maximum: 50}
+	validates :secretary, presence: true, length: { maximum: 50}
+	validates :treasurer, presence: true, length: { maximum: 50}
+
+	def eboard_params
+      params.require(:eboard).permit(:president, :vice_president, :secretary,
+                                   :treasurer)
+  	end
+
 end
